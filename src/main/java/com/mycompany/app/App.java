585fc83050d53527270a05624436d3100cf3c81e
sang5052003 <sang5052003@naver.com>
2018-04-25 07:24:54 +0000

    3rd try

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! Changed push 2nd try";
+    private final String message = "Hello World! Changed push 3rd try";
 
     public App() {}
 
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,7 +34,7 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello World! Changed push 2nd try" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello World! Changed push 3rd try" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
             fail("\"message\" is not \"Hello World!\"");
         }
