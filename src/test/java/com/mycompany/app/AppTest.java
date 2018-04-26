733660ffc68ea06356e28c4fb72dff06b6171375
sang5052003 <sang5052003@naver.com>
2018-04-26 08:36:11 +0000

    slack test

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,7 +34,7 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello World! Changed push 9th try" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello World! slack test" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
             fail("\"message\" is not \"Hello World!\"");
         }
