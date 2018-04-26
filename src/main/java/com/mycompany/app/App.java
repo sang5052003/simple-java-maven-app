8b48a017744a82d949cd0d88898672b3b2c47232
sang5052003 <sang5052003@naver.com>
2018-04-26 09:15:56 +0000

    slack test 3rd

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! slack test 2nd";
+    private final String message = "Hello World! slack test 3rd";
 
     public App() {}
 
